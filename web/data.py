f9debd8a081192b86fc57fd8ab7df3e2aa78e51b
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-04-04 18:59:50 +0800

    refactor: web.data.get_ph_numbers_delta

--- web/data.py
+++ web/data.py
@@ -90,45 +90,30 @@ def get_ph_numbers():
 
 
 def get_ph_numbers_delta():
-    time_conf_unique = get_confirmed_over_time()
-    time_recov_unique = get_recovered_over_time()
-    time_dead_unique = get_deaths_over_time()
     numbers = get_ph_numbers()
+    case_names = [
+        'confirmed',
+        'recovered',
+        'deaths',
+    ]
+    time_unique = [
+        get_confirmed_over_time(),
+        get_recovered_over_time(),
+        get_deaths_over_time(),
+    ]
+    delta = []
 
-    ph_time = time_conf_unique.query("`Country/Region` == 'Philippines'")
-    ph_time = ph_time[ph_time.columns[4:]]
-    delta_conf = [0, *np.diff(ph_time.values.squeeze())]
-    conf_time = ph_time.copy().columns
-    today_count_conf = numbers.query("`type` == 'confirmed'")['count'].values[0]
-    remote_count = ph_time[conf_time[-1]].values[0]
-    if today_count_conf == remote_count:
-        remote_count = ph_time[conf_time[-2]].values[0]
-    delta_conf = today_count_conf - remote_count
-
-    ph_time = time_recov_unique.query("`Country/Region` == 'Philippines'")
-    ph_time = ph_time[ph_time.columns[4:]]
-    delta_recov = [0, *np.diff(ph_time.values.squeeze())]
-    recov_time = ph_time.copy().columns
-    today_count_recov = numbers.query("`type` == 'recovered'")['count'].values[0]
-    remote_count = ph_time[recov_time[-1]].values[0]
-    if today_count_recov == remote_count:
-        remote_count = ph_time[recov_time[-2]].values[0]
-    delta_recov = today_count_recov - remote_count
-
-    ph_time = time_dead_unique.query("`Country/Region` == 'Philippines'")
-    ph_time = ph_time[ph_time.columns[4:]]
-    delta_dead = [0, *np.diff(ph_time.values.squeeze())]
-    dead_time = ph_time.copy().columns
-    today_count_dead = numbers.query("`type` == 'deaths'")['count'].values[0]
-    remote_count = ph_time[dead_time[-1]].values[0]
-    if today_count_dead == remote_count:
-        remote_count = ph_time[dead_time[-2]].values[0]
-    delta_dead = today_count_dead - remote_count
-    return {
-        'delta_conf': delta_conf,
-        'delta_recov': delta_recov,
-        'delta_dead': delta_dead,
-    }
+    for unique, name in zip(time_unique, case_names):
+        ph_time = unique.query("`Country/Region` == 'Philippines'")
+        ph_time = ph_time[ph_time.columns[4:]]
+        delta = [0, *np.diff(ph_time.values.squeeze())]
+        time = ph_time.copy().columns
+        today_count = numbers.query(f"`type` == '{name}'")['count'].values[0]
+        remote_count = ph_time[time[-1]].values[0]
+        if today_count == remote_count:
+            remote_count = ph_time[time[-2]].values[0]
+        delta.append(today_count - remote_count)
+    return dict(zip(case_names, delta))
 
 
 def get_ph_hospitals():
