ef91fba792235b2c262beab9edc8a9917e2d4096
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-07-10 16:44:18 +0800

    Init React port bootstrap Django + webpack

--- ncovenience/urls.py
+++ ncovenience/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('backend.urls')),
+    path('beta/', include('frontend.urls')),
     path('', include('web.urls')),
 ]
